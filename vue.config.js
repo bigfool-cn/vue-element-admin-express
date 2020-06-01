e6c1d7042c31c6b16897b9f6ac71f2924612cf2b
bigfool-cn <1063944784@qq.com>
2020-06-01 11:01:37 +0800

    update vue.config.js. v1.0.0

--- vue.config.js
+++ vue.config.js
@@ -38,7 +38,7 @@ module.exports = {
     },
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // 后台接口域名
+        target: 'http://127.0.0.1:3001', // 后台接口域名
         ws: true, // 如果要代理 websockets，配置这个参数
         secure: false, // 如果是https接口，需要配置这个参数
         changeOrigin: true, // 是否跨域
